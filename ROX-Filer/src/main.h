6bfddce6e1e398fe982dbf0547ede9ca591d3a03
Thomas Leonard <talex5@gmail.com>
1999-07-29 15:47:31 +0000

    r37: Added these files to CVS.

---
+++ ROX-Filer/src/main.h
@@ -0,0 +1,16 @@
+/* vi: set cindent:
+ * $Id$
+ *
+ * ROX-Filer, filer for the ROX desktop project
+ * By Thomas Leonard, <tal197@ecs.soton.ac.uk>.
+ */
+
+#ifndef _MAIN_H
+#define _MAIN_H
+
+extern int number_of_windows;
+
+/* Prototypes */
+int main(int argc, char **argv);
+
+#endif /* _MAIN_H */
