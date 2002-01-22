0f5900572c3580e461aebddfe18f24b68e2aa7b0
Thomas Leonard <talex5@gmail.com>
2002-01-22 13:22:29 +0000

    r1115: Bugfix: Under Gtk+-2.0, the savebox focussed the OK button rather than the 	text entry.

--- ROX-Filer/src/gtksavebox.c
+++ ROX-Filer/src/gtksavebox.c
@@ -238,7 +238,11 @@ gtk_savebox_init (GtkSavebox *savebox)
 		      GTK_SIGNAL_FUNC (do_save), savebox);
   gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
   GTK_WIDGET_SET_FLAGS (button, GTK_CAN_DEFAULT);
+#ifdef GTK2
+  gtk_widget_grab_default (button);
+#else
   gtk_window_set_default (GTK_WINDOW (savebox), button);
+#endif
 
   button = gtk_button_new_with_label (_("Cancel"));
   gtk_signal_connect (GTK_OBJECT (button), "clicked",
