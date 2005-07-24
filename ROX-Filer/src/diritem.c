932aad31b327ad51e6008dd95a0314c7ef765a47
Thomas Leonard <talex5@gmail.com>
2005-07-24 10:03:55 +0000

    r4033: An executable text file of unknown type is marked as executable if its name doesn't contain a '.'. This is so that 'readme.txt' on a filesystem with all execute bits set is seen as text, but a shell script with no extension or interpreter header still shows up as an executable on Solaris (Thomas Leonard; requested by Stephen Watson).

--- ROX-Filer/src/diritem.c
+++ ROX-Filer/src/diritem.c
@@ -177,8 +177,13 @@ void diritem_restat(const guchar *path, DirItem *item, struct stat *parent)
 			 */
 			item->flags |= ITEM_FLAG_EXEC_FILE;
 
-			if (item->mime_type == NULL || item->mime_type == application_octet_stream)
+			if (item->mime_type == NULL ||
+			    item->mime_type == application_octet_stream ||
+			    (item->mime_type == text_plain &&
+			     !strchr(item->leafname, '.')))
+			{
 				item->mime_type = application_executable;
+			}
 		}		
 
 		if (!item->mime_type)
